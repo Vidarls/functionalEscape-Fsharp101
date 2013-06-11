e3fc1e373e63b22da55ff498d1f79b2469f2a796
Vidar L. Sømme <vidar@vetserve.no>
2013-06-11 22:08:40 +0200

    Value objects, the tiresome way
    Value object in C#
    
    - Failing Equality tests

---
+++ src/fsharp101/theLongWindedWay/Name.cs
@@ -0,0 +1,29 @@
+﻿namespace theLongWindedWay
+{
+    public class Name
+    {
+        private readonly string _lastName;
+        private readonly string _firstName;
+
+        public string LastName
+        {
+            get { return _lastName; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+        }
+
+        public Name(string lastName, string firstName)
+        {
+            _lastName = lastName;
+            _firstName = firstName;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", FirstName, LastName);
+        }
+    }
+}
---
+++ src/fsharp101/theLongWindedWay/Tests/ValueObjectTests.cs
@@ -0,0 +1,19 @@
+﻿using Ploeh.AutoFixture.Xunit;
+using Xunit;
+using Xunit.Extensions;
+
+// ReSharper disable InconsistentNaming
+namespace theLongWindedWay.Tests
+{
+    public class ValueObjectTests
+    {
+        [Theory, AutoData]
+        public void Two_objects_with_the_same_names_should_be_equal(string lastName, string firstName)
+        {
+            var nameInstanceOne = new Name(lastName, firstName);
+            var nameInstanceTwo = new Name(lastName, firstName);
+
+            Assert.Equal(nameInstanceOne, nameInstanceTwo);
+        }
+    }
+}
